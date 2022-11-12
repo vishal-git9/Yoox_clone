7c4a5a2124c935fc7b60b8fa385bdac5255050f8
nitishmandal01 <neelgiri65@gmail.com>
2022-11-13 01:00:54 +0530

    art & design

--- kids.js
+++ kids.js
@@ -3,3 +3,9 @@ import { navbar } from "./components/navbar.js";
 let navbar_div=document.getElementById("navbar");
 navbar_div.innerHTML=navbar();
 
+
+import { footer } from "./components/footer.js";
+
+let footer_part = document.getElementById("footer-part")
+footer_part.innerHTML  = footer()
+// console.log("hi")
